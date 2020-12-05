956f0a642522c5b51b20c6797f16c72f5c70b6b8
jlmolina <j.l.molina@cgiar.org>
2020-12-05 09:17:35 -0500

    metodo

--- handler.js
+++ handler.js
@@ -1,18 +1,15 @@
 'use strict';
-
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.miMetodoClase = async event => {
+    let nombre = event.queryStringParameters != null ?
+        event.queryStringParameters.nombre : null
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message: 'Hola ${nombre}'
+            },
+            null,
+            2
+        ),
+    };
 };
