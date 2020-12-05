7728f401cb0defebe7c928aa6a46dc2c99ab28a1
jlmolina <j.l.molina@cgiar.org>
2020-12-05 10:57:52 -0500

    getAll Dynamo

---
+++ controllers/data.js
@@ -0,0 +1,16 @@
+'use strict'
+const DynamoHelper = require('../resources/dynamo_helper')
+const constantes = require('../resources/constantes')
+const dynamo = new DynamoHelper()
+async function getAllProducts () {
+  console.log(`${process.env.LOG_ENVIRONMENT} -> start getAllProducts...`)
+  const params = {
+    TableName: process.env.TABLE_PRODUCTS
+  }
+  const data = await dynamo.loadData(params)
+  console.log(`${process.env.LOG_ENVIRONMENT} -> end getAllProducts successfull with ${JSON.stringify(data)}`)
+  return data.Items
+}
+module.exports = {
+  getAllProducts
+}
--- handler.js
+++ handler.js
@@ -1,15 +1,26 @@
 'use strict';
-module.exports.miMetodoClase = async event => {
-    let nombre = event.queryStringParameters != null ?
-        event.queryStringParameters.nombre : null
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: `Hola ${nombre}`
-            },
-            null,
-            2
-        ),
-    };
-};
+const data = require('./controllers/data')
+const constantes = require('./resources/constantes')
+const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, HEAD, OPTIONS'
+}
+const sendResponse = (statusCode, body) => {
+    const response = {
+        statusCode: statusCode,
+        headers: headers,
+        body: body
+    }
+    return response
+}
+module.exports.getAll = async (event, context) => {
+    try{
+        console.log(`init getAll productos ${JSON.stringify(event)}`)
+        const datos = await data.getAllProducts()
+        return sendResponse(constantes.SUCESSFULL_EXECUTION, JSON.stringify(datos))
+    } catch (error){
+        console.error(error)
+        return sendResponse(constantes.SERVER_ERROR, JSON.stringify(error))
+    }
+}
